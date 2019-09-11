1cc352e4adc7d04c2d7e7ebdcc72d7b57bc8a1ce
Richard Riley <rileyrg@gmail.com>
2019-09-11 06:53:08 +0200

    in a right mess with PIO

---
+++ projects/DigitalInputs/.dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-mode
+  (projectile-project-compilation-dir . "build")
+  (projectile-project-compilation-cmd . "cmake .. && make DigitalInputs_flash")))
+
---
+++ projects/DigitalInputs/src/main.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include <Arduino.h>
+#include <avr/io.h>
+
+#define  ledPin 5
+#define  buttonApin 9
+#define  buttonBpin 8
+
+byte leds = 0;
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonApin, INPUT_PULLUP);  
+  pinMode(buttonBpin, INPUT_PULLUP);  
+}
+
+void loop() 
+{
+  static boolean pressed=false,  on=false; //
+  if (digitalRead(buttonApin) == LOW)
+  {
+    if (!pressed) {
+      pressed=true;
+      digitalWrite(ledPin, (on=!on)?HIGH:LOW);
+    }
+  } else
+    pressed=false;
+}
