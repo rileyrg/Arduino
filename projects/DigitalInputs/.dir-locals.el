a5c2974499d9e424a90b038bc9e5066c68cb551f
Richard Riley <rileyrg@gmail.com>
2019-09-11 07:38:44 +0200

    mess

--- projects/DigitalInputs/.dir-locals.el
+++
@@ -1,7 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((c++-mode
-  (projectile-project-compilation-dir . "build")
-  (projectile-project-compilation-cmd . "cmake .. && make DigitalInputs_flash")))
-
